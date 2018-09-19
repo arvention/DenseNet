03893e5412dbcf093e02ef35650ff458ed8947f4
arvention <amc.antioquia@yahoo.com.ph>
2018-09-19 22:36:09 +0800

    finished methods train, eval, train_evaluate, and test

--- solver.py
+++ solver.py
@@ -4,6 +4,8 @@ import time
 import datetime
 import torch.nn as nn
 import torch.optim as optim
+from tqdm import tqdm
+from utils import to_var
 
 from model import DenseNet
 
@@ -121,13 +123,6 @@ class Solver(object):
         )
         torch.save(self.model.state_dict(), path)
 
-    def train(self):
-        """
-        Training process
-        """
-        # TODO: add training process
-        pass
-
     def model_step(self, images, labels):
         """
         A step for each iteration
@@ -155,18 +150,61 @@ class Solver(object):
         # return loss
         pass
 
-    def train_evaluate(self, e):
+    def train(self):
         """
-        Evaluates the performance of the model using the train dataset
+        Training process
         """
-        # TODO: call self.eval() then print log
+        # TODO: add training process
+        self.losses = []
+        self.top_1_acc = []
+        self.top_5_acc = []
 
-    def test(self):
-        """
-        Evaluates the performance of the model using the test dataset
-        """
-        # TODO: call self.eval() then print log
-        pass
+        iters_per_epoch = len(self.data_loader)
+
+        # start with a trained model if exists
+        if self.pretrained_model:
+            start = int(self.pretrained_model.split('_')[1]) - 1
+        else:
+            start = 0
+
+        # start training
+        start_time = time.time()
+        for e in range(start, self.num_epochs):
+            for i, (images, labels) in enumerate(tqdm(self.data_loader)):
+                images = to_var(images, self.use_gpu)
+                labels = to_var(labels, self.use_gpu)
+
+                loss = self.model_step(images, labels)
+
+            # print out log loss
+            if (e + 1) % self.loss_log_step == 0:
+                self.print_loss_log(start_time, iters_per_epoch, e, i, loss)
+                self.losses.append((e, loss))
+
+            # save model
+            if (e + 1) % self.model_save_step == 0:
+                self.save_model(e, i)
+
+            # evaluate on train dataset
+            if (e + 1) % self.train_eval_step == 0:
+                top_1_acc, top_5_acc = self.train_evaluate(e)
+                self.top_1_acc.append((e, top_1_acc))
+                self.top_5_acc.append((e, top_5_acc))
+
+        # print losses
+        print('\n--Losses--')
+        for e, loss in self.losses:
+            print(e, '{:.4f}'.format(loss))
+
+        # print top_1_acc
+        print('\n--Top 1 accuracy--')
+        for e, acc in self.top_1_acc:
+            print(e, '{:.4f}'.format(acc))
+
+        # print top_5_acc
+        print('\n--Top 5 accuracy--')
+        for e, acc in self.top_5_acc:
+            print(e, '{:.4f}'.format(acc))
 
     def eval(self, data_loader):
         """
@@ -174,6 +212,57 @@ class Solver(object):
         """
 
         # set the model to eval mode
-        # TODO: self.model.eval()
+        self.model.eval()
 
-        # TODO: return evaluation metric
+        top_1_correct = 0
+        top_5_correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for images, labels in data_loader:
+
+                images = to_var(images, self.use_gpu)
+                labels = to_var(labels, self.use_gpu)
+
+                output = self.model(images)
+                total += labels.size()[0]
+
+                # top 1
+                # get the max for each instance in the batch
+                _, top_1_output = torch.max(output.data, dim=1)
+
+                top_1_correct += torch.sum(torch.eq(labels.squeeze(),
+                                                    top_1_output))
+
+                # top 5
+                _, top_5_output = torch.topk(output.data, k=5, dim=1)
+                for i, label in enumerate(labels):
+                    if label in top_5_output[i]:
+                        top_5_correct += 1
+
+        return top_1_correct.item(), top_5_correct, total
+
+    def train_evaluate(self, e):
+        """
+        Evaluates the performance of the model using the train dataset
+        """
+        top_1_correct, top_5_correct, total = self.eval(self.data_loader)
+        log = "Epoch[{}/{}]--top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
+            e + 1,
+            self.num_epochs,
+            top_1_correct / total,
+            top_5_correct / total
+        )
+        print(log)
+        return top_1_correct / total, top_5_correct / total
+
+    def test(self):
+        """
+        Evaluates the performance of the model using the test dataset
+        """
+        top_1_correct, top_5_correct, total = self.eval(self.data_loader)
+        log = "top_1_acc: {:.4f}--top_5_acc: {:.4f}".format(
+            top_1_correct / total,
+            top_5_correct / total
+        )
+        print(log)
